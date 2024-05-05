d5521b2199cd4654237c64cdc47903c6389f9760
unknown <linyanyu99@outlook.com>
2024-05-05 18:21:29 -0400

    initial code to generate titles using a pretrained model, somehow notebook can't work on my pc, so check settings

--- main.py
+++ main.py
@@ -2,15 +2,87 @@ import pandas as pd
 import argparse
 import pandas as pd
 import torch
-# from transformers import RobertaTokenizer, RobertaForMaskedLM
+from transformers import RobertaTokenizer, RobertaForMaskedLM
 import numpy as np
-# import torch.nn as nn
-# import torch.optim as optim
+import torch.nn as nn
+import torch.optim as optim
 
 from matplotlib import pyplot as plt
 
-# from transformers import EncoderDecoderModel, BertTokenizer
+from transformers import EncoderDecoderModel, BertTokenizer
+from transformers import AutoModel
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+
 from sklearn.model_selection import train_test_split
+
+# Define a simple Encoder
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, (hidden, cell) = self.rnn(embedded)
+        return hidden, cell
+
+# Define a simple Decoder
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+        self.fc_out = nn.Linear(hid_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input, hidden, cell):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        prediction = self.fc_out(output.squeeze(0))
+        return prediction, hidden, cell
+    
+# Define the seq2seq model
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        
+    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+        
+        hidden, cell = self.encoder(src)
+        
+        input = trg[0,:]
+        
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(input, hidden, cell)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+        
+        return outputs  
+
+def generateTitle(sentence, tokenizer, model, device):
+    input_text = sentence
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
+
+    outputs = model.generate(input_ids)
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    print("title:", summary)
 
 def main():
     ###### SETUP ############################
@@ -18,6 +90,16 @@ def main():
     dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_All_Beauty", trust_remote_code=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
+    tokenizer = AutoTokenizer.from_pretrained("czearing/article-title-generator")
+    model = T5ForConditionalGeneration.from_pretrained("czearing/article-title-generator")
+    model.to(device)
+    for i in range(20):
+        sent = dataset['full'][i]['text']
+        print("\n", sent)
+        print(dataset['full'][i]['title'])
+        generateTitle(sent, tokenizer, model, device)
+        # print()
+
 
 if __name__ == '__main__':
     main()
