470548e76f0defe80f6bfa9688fb0e3f3fedad40
unknown <linyanyu99@outlook.com>
2024-05-08 22:49:10 -0400

    last group of images

--- main.py
+++ main.py
@@ -14,66 +14,66 @@ from transformers import AutoModel
 from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 from sklearn.model_selection import train_test_split
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# # Define a simple Encoder
+# class Encoder(nn.Module):
+#     def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+#         super().__init__()
+        
+#         self.embedding = nn.Embedding(input_dim, emb_dim)
+#         self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+#         self.dropout = nn.Dropout(dropout)
+        
+#     def forward(self, src):
+#         embedded = self.dropout(self.embedding(src))
+#         outputs, (hidden, cell) = self.rnn(embedded)
+#         return hidden, cell
 
-# Define a simple Encoder
-class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
+# # Define a simple Decoder
+# class Decoder(nn.Module):
+#     def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+#         super().__init__()
         
-        self.embedding = nn.Embedding(input_dim, emb_dim)
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
-        self.dropout = nn.Dropout(dropout)
+#         self.embedding = nn.Embedding(output_dim, emb_dim)
+#         self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+#         self.fc_out = nn.Linear(hid_dim, output_dim)
+#         self.dropout = nn.Dropout(dropout)
         
-    def forward(self, src):
-        embedded = self.dropout(self.embedding(src))
-        outputs, (hidden, cell) = self.rnn(embedded)
-        return hidden, cell
-
-# Define a simple Decoder
-class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
-        
-        self.embedding = nn.Embedding(output_dim, emb_dim)
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
-        self.fc_out = nn.Linear(hid_dim, output_dim)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, input, hidden, cell):
-        input = input.unsqueeze(0)
-        embedded = self.dropout(self.embedding(input))
-        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
-        prediction = self.fc_out(output.squeeze(0))
-        return prediction, hidden, cell
+#     def forward(self, input, hidden, cell):
+#         input = input.unsqueeze(0)
+#         embedded = self.dropout(self.embedding(input))
+#         output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+#         prediction = self.fc_out(output.squeeze(0))
+#         return prediction, hidden, cell
     
-# Define the seq2seq model
-class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder, device):
-        super().__init__()
+# # Define the seq2seq model
+# class Seq2Seq(nn.Module):
+#     def __init__(self, encoder, decoder, device):
+#         super().__init__()
         
-        self.encoder = encoder
-        self.decoder = decoder
-        self.device = device
+#         self.encoder = encoder
+#         self.decoder = decoder
+#         self.device = device
         
-    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
-        batch_size = trg.shape[1]
-        trg_len = trg.shape[0]
-        trg_vocab_size = self.decoder.output_dim
+#     def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+#         batch_size = trg.shape[1]
+#         trg_len = trg.shape[0]
+#         trg_vocab_size = self.decoder.output_dim
         
-        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+#         outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
         
-        hidden, cell = self.encoder(src)
+#         hidden, cell = self.encoder(src)
         
-        input = trg[0,:]
+#         input = trg[0,:]
         
-        for t in range(1, trg_len):
-            output, hidden, cell = self.decoder(input, hidden, cell)
-            outputs[t] = output
-            teacher_force = random.random() < teacher_forcing_ratio
-            top1 = output.argmax(1)
-            input = trg[t] if teacher_force else top1
+#         for t in range(1, trg_len):
+#             output, hidden, cell = self.decoder(input, hidden, cell)
+#             outputs[t] = output
+#             teacher_force = random.random() < teacher_forcing_ratio
+#             top1 = output.argmax(1)
+#             input = trg[t] if teacher_force else top1
         
-        return outputs  
+#         return outputs  
 
 def generateTitle(sentence, tokenizer, model, device):
     input_text = sentence
@@ -82,24 +82,38 @@ def generateTitle(sentence, tokenizer, model, device):
     outputs = model.generate(input_ids)
     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    print("title:", summary)
+    # print("Generated:", summary)
+    return summary
 
 def main():
     ###### SETUP ############################
     from datasets import load_dataset
     dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_All_Beauty", trust_remote_code=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
+    # print(device)
     tokenizer = AutoTokenizer.from_pretrained("czearing/article-title-generator")
     model = T5ForConditionalGeneration.from_pretrained("czearing/article-title-generator")
     model.to(device)
-    for i in range(20):
+    sentimentAnalyser = SentimentIntensityAnalyzer()
+    sent_score_ori = []
+    sent_score_gen = []
+    for i in range(200):
         sent = dataset['full'][i]['text']
-        print("\n", sent)
-        print(dataset['full'][i]['title'])
-        generateTitle(sent, tokenizer, model, device)
-        # print()
-
+        # print("\n", sent)
+        # print("Title: ", dataset['full'][i]['title'])
+        ori = dataset['full'][i]['title']
+        gen = generateTitle(sent, tokenizer, model, device)
+        scores = sentimentAnalyser.polarity_scores(ori)
+        # print(scores)
+        sent_score_ori.append(scores['compound'])
+        
+        scores = sentimentAnalyser.polarity_scores(gen)
+        sent_score_gen.append(scores['compound'])
+        # if scores['compound'] == 0:
+        #     print(ori, gen)
+        
+    plt.scatter(sent_score_ori, sent_score_gen, s=5)
+    plt.savefig("sent.png")
 
 if __name__ == '__main__':
     main()
